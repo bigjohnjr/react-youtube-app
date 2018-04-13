d9b948b1fefa68dc5587752e662588d342d61afb
John White <jaydub@John-Whites-MacBook-Pro.local>
2018-04-12 19:23:10 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
+import './index.css';
+const API_KEY = 'AIzaSyB-IjPRS2c9yUYAfmFB3keYb7pQCoxDW9c';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('puppies');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+    return (
+      <div>
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
